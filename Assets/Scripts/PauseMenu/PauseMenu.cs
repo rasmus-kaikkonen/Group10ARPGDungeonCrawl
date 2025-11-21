da0d65cc62f910e518eb55631a2a5413c2489ff3
Rasmus Kaikkonen <157014167+rasmus-kaikkonen@users.noreply.github.com>
2025-11-21 12:05:16 +0200

    PauseMenu Basic Impl

--- Assets/Scripts/PauseMenu/PauseMenu.cs
+++ Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] Canvas pauseMenu;
+    public bool isPaused = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current[Key.Escape].isPressed)
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.enabled = true;
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+    public void ContinueGame()
+    {
+        pauseMenu.enabled = false;
+        Time.timeScale = 1;
+        isPaused = true;
+    }
+    public void ExitToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }

919f2469d18701fd8fa18733e138d0d892bcd352
JouniHeimonen <jouni.heimonen@hotmail.fi>
2025-11-26 13:05:03 +0200

    päivitys freecamiin

--- Assets/Scripts/Player/CamControl.cs
+++ Assets/Scripts/Player/CamControl.cs
@@ -4,42 +4,26 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-/// <summary>
-/// A simple free camera to be added to a Unity game object.
-/// 
-/// Keys:
-///	arrows	- movement
-///	hold shift		- enable fast movement mode
-///	hold alt  	- enable free look
-///     
-/// </summary>
+
+/*  Keys:
+	arrows	- movement
+	hold shift		- enable fast movement mode
+	hold alt  	- enable free look
+*/    
+
 public class FreeCam : MonoBehaviour
 {
-    /// <summary>
-    /// Normal speed of camera movement.
-    /// </summary>
+   
     public float movementSpeed = 10f;
 
-    /// <summary>
-    /// Speed of camera movement when shift is held down,
-    /// </summary>
-    public float fastMovementSpeed = 100f;
-
-    /// <summary>
-    /// Sensitivity for free look.
-    /// </summary>
+ 
     public float freeLookSensitivity = 3f;
 
 
-    /// <summary>
-    /// Set to true when free looking (on right mouse button).
-    /// </summary>
     private bool looking = false;
 
     void Update()
     {
-        var fastMode = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-        var movementSpeed = fastMode ? this.fastMovementSpeed : this.movementSpeed;
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -61,7 +45,6 @@ public class FreeCam : MonoBehaviour
             transform.position = transform.position + (-transform.up * movementSpeed * Time.deltaTime);
         }
 
-       
 
         if (looking)
         {
@@ -69,8 +52,7 @@ public class FreeCam : MonoBehaviour
             float newRotationY = Input.GetAxis("Vertical") * freeLookSensitivity;
             transform.localEulerAngles = new Vector3(newRotationY, newRotationX, 0f);
         }
-
-        
+ 
 
         if (Input.GetKeyDown(KeyCode.LeftAlt))
         {
@@ -87,18 +69,14 @@ public class FreeCam : MonoBehaviour
         StopLooking();
     }
 
-    /// <summary>
-    /// Enable free looking.
-    /// </summary>
+    
     public void StartLooking()
     {
         looking = true;
        
     }
 
-    /// <summary>
-    /// Disable free looking.
-    /// </summary>
+    
     public void StopLooking()
     {
         looking = false;
